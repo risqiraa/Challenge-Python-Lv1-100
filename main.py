939dfe521be693f403a600a3da4769759e285900
Risqi RA <risqiraacc@gmail.com>
2023-07-17 00:45:33 +0700

    Level 15 Tulis program Python yang mencetak deret angka Fibonacci hingga suatu batas yang ditentukan oleh pengguna.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 
-def level14():
-    jumlah_kata = 0
-    kata = input("masukkan kalimat anda: ")
-    set_kata = set(kata.lower().split())
-    print(set_kata)
-    for i in set_kata:
-        if len(i) == 3:
-            jumlah_kata +=1
+def level15():
+    hasil_f=[0,1]
+    A1, A2 = 0,1
+    max_angka = int(input("masukkan maksimal angka: "))
+    while A2 <= max_angka:
+        A1,A2 = A2, A1+A2
+        if A2 >= max_angka:
+            break
+        hasil_f.append(A2)
 
-    print("Jumlah Kata 3huruf: "+str(jumlah_kata))
+    print(hasil_f)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    level14()
+    level15()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
