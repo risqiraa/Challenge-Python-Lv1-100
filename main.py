e36414c9ace32c6c6320fbdd4e4aba2db94c154f
Risqi RA <risqiraacc@gmail.com>
2023-07-17 00:50:15 +0700

    Level 16 Tulis program Python yang meminta pengguna untuk memasukkan dua angka. Program harus mencetak hasil perkalian dari dua angka tersebut. Namun, jika hasil perkalian lebih dari 1000, program harus mencetak "Hasil terlalu besar!".

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 
-def level15():
-    hasil_f=[0,1]
-    A1, A2 = 0,1
-    max_angka = int(input("masukkan maksimal angka: "))
-    while A2 <= max_angka:
-        A1,A2 = A2, A1+A2
-        if A2 >= max_angka:
-            break
-        hasil_f.append(A2)
-
-    print(hasil_f)
-
-
+def level16():
+    angka1 = int(input("Masukkan angka 1: "))
+    angka2 = int(input("Masukkan angka 2: "))
+    hasil_kali = angka1*angka2
+    if hasil_kali > 1000:
+        print("Hasil: "+str(hasil_kali)+", Hasil Teralu Besar")
+    else:
+        print("Hasil: "+str(hasil_kali)+", Hasil Dalam Range")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    level15()
+    level16()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
