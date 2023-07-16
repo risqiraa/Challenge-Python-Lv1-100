5953e5416ab1ba3e5ebd12a8c71fa0b499fcdecf
Risqi RA <risqiraacc@gmail.com>
2023-07-16 21:05:46 +0700

    Level 1 Cetak Hello World

---
+++ main.py
@@ -0,0 +1,8 @@
+def printName():
+    print("Hello World")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    printName()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
