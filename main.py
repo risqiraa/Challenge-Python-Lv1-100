8c0f69d8b376ff5e026cedabec5a9a7fafc414b7
Risqi RA <risqiraacc@gmail.com>
2023-07-17 00:19:54 +0700

    Level 14 Tulis program Python yang menghitung jumlah kata yang terdiri dari tiga huruf dalam sebuah kalimat yang dimasukkan oleh pengguna. Sebuah kalimat dianggap sebagai urutan karakter yang dapat mengandung spasi.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 
-def level13():
-    kata = input("masukkan kata anda: ")
-    kata_unik = len(set(kata.lower().split()))
-    print("Jumlah Kata Unik: "+str(kata_unik))
+def level14():
+    jumlah_kata = 0
+    kata = input("masukkan kalimat anda: ")
+    set_kata = set(kata.lower().split())
+    print(set_kata)
+    for i in set_kata:
+        if len(i) == 3:
+            jumlah_kata +=1
+
+    print("Jumlah Kata 3huruf: "+str(jumlah_kata))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    level13()
+    level14()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
