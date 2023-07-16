d675a78fcc30fd74324d41657299fdf7d4aa959c
Risqi RA <risqiraacc@gmail.com>
2023-07-16 23:20:13 +0700

    Level 9 Tulis program Python yang menghitung berapa kali setiap kata muncul dalam sebuah teks yang dimasukkan oleh pengguna. Sebuah kata dianggap sebagai urutan karakter yang tidak mengandung spasi.

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-def level8():
-    angka1 = input("input susunan angka1 (pisahkan dengan koma): ")
-    angka2 = input("input susunan angka2 (pisahkan dengan koma): ")
-    listAngka1 = angka1.split(",")
-    listAngka2 = angka2.split(",")
-    listAngka1 = [int(angka) for angka in listAngka1]
-    listAngka2 = [int(angka) for angka in listAngka2]
-    gabungan = sorted(list(set(listAngka1+listAngka2)))
+from collections import Counter
+def level9():
+    word = input("Masukkan kalimat anda: ")
+    word = word.lower().split()
+    word_count = Counter(word)
+    word_count1 = word_count.most_common(2)
+    print(word_count)
+    print(word_count1)
 
-    print("List angka1: "+str(listAngka1))
-    print("List angka2: "+str(listAngka2))
-    print("Gabungan 2 Susunan angka tersebut adalah: "+str(gabungan))
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    level8()
+    level9()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
