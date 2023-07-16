a1af4359732b6839346c438cccd1d86eeb4820f0
Risqi RA <risqiraacc@gmail.com>
2023-07-16 21:13:22 +0700

    Level 2 Tulis program Python yang mengambil dua angka dari pengguna dan mencetak hasil penjumlahan keduanya.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def printName():
-    print("Hello World")
-
+def level2():
+    angka1 = input("Silahkan input angka ke-1: ")
+    angka1 = int(angka1)
+    angka2 = input("Silahkan input angka ke-2: ")
+    angka2 = int(angka2)
+    jumlah = angka1+angka2
+    print("Hasil penjumlahan dari "+str(angka1)+" dan "+str(angka2)+" adalah "+str(jumlah))
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    printName()
+    level2()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
