2e07da8d6b80a0bbab3e0cdc5b18ceab7adb9c27
Risqi RA <risqiraacc@gmail.com>
2023-07-16 23:36:36 +0700

    Level 10 Tulis program Python yang mengubah setiap kata dalam sebuah kalimat menjadi kata dengan huruf kapital pertama.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from collections import Counter
-def level9():
-    word = input("Masukkan kalimat anda: ")
-    word = word.lower().split()
-    word_count = Counter(word)
-    word_count1 = word_count.most_common(2)
-    print(word_count)
-    print(word_count1)
 
+def level10():
+    kalimat = input("masukkan paragraf anda: ")
+    kalimat = kalimat.lower()
+    print(str(kalimat))
+    kalimat = kalimat.capitalize()
+    print(str(kalimat))
+    kalimat = kalimat.upper()
+    print(str(kalimat))
+    kalimat = kalimat.title()
+    print(str(kalimat))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    level9()
+    level10()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
