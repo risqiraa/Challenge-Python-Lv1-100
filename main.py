82f150220a01ccb9b8385199d8a3a33714feb722
Risqi RA <risqiraacc@gmail.com>
2023-07-16 22:14:26 +0700

    Level 7 Tulis program Python yang menghitung jumlah kata dalam sebuah kalimat yang dimasukkan oleh pengguna. Sebuah kata dianggap sebagai urutan karakter yang tidak mengandung spasi.

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-def level6():
-
-    angka1 = input("masukkan angka ke-1: ")
-    angka2 = input("masukkan angka ke-2: ")
-    angka1 = int(angka1)
-    angka2 = int(angka2)
-    numbers = []
-    for number in range(angka1, angka2 + 1):
-        if number > 1:
-            is_prime = True
-            for i in range(2, number):
-                if number % i == 0:
-                    is_prime = False
-                    break
-            if is_prime:
-                numbers.append(number)
-
-    print("Bilangan Prima Antara " + str(angka1) + " dan " + str(angka2) + " adalah: " + str(numbers))
-
-
-
+def level7():
+    text = input("masukkan kalimat: ")
+    word = text.split()
+    jumlah_kata = len(word)
+    print("jumlah kata pada kalimat "+'"'+text+'"'+ " adalah "+str(jumlah_kata)+" kata")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    level6()
+    level7()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
