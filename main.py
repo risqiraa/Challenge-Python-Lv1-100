19c64f6b35e0753982cc2efca158c3c40eb616ea
Risqi RA <risqiraacc@gmail.com>
2023-07-16 23:49:51 +0700

    Level 11 Tulis program Python yang menghitung jumlah huruf vokal dan konsonan dalam sebuah kata yang dimasukkan oleh pengguna. Sebuah kata dianggap sebagai urutan karakter yang tidak mengandung spasi.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 
-def level10():
-    kalimat = input("masukkan paragraf anda: ")
-    kalimat = kalimat.lower()
-    print(str(kalimat))
-    kalimat = kalimat.capitalize()
-    print(str(kalimat))
-    kalimat = kalimat.upper()
-    print(str(kalimat))
-    kalimat = kalimat.title()
-    print(str(kalimat))
+def level11():
+    hitung_vokal = 0
+    hitung_konsonan = 0
+    kata = input("masukkan kata anda: ")
+    kata = list(kata.lower())
+    print (kata)
+    for huruf in kata:
+        if huruf.lower() in ["a","i","u","e","o"]:
+            hitung_vokal +=1
+        else:
+            hitung_konsonan +=1
 
+    print("Vokal: "+str(hitung_vokal)+"\nKonsonan: "+str(hitung_konsonan))
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    level10()
+    level11()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
